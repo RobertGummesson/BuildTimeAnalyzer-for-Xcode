451e2d39443019fda5de17cb5621f0f35be3d5f2
Robert Gummesson <robert@canemedia.co.uk>
2016-09-14 16:35:30 +0100

    Got rid of some unused code

--- BuildTimeAnalyzer/DerivedDataManager.swift
+++ BuildTimeAnalyzer/DerivedDataManager.swift
@@ -32,41 +32,4 @@ class DerivedDataManager {
         
         return enumerator.map{ $0 as! URL }
     }
-    
-    static func listCacheFiles() -> [CacheFile] {
-        let files = cacheFiles(at: URL(fileURLWithPath: UserSettings.derivedDataLocation))
-        let earliestDate = Date().addingTimeInterval(-24 * 60 * 60)
-        return filterFiles(files, byEarliestDate: earliestDate)
-    }
-    
-    static private func cacheFiles(at url: URL) -> [CacheFile] {
-        let fileManager = FileManager.default
-        let keys = [URLResourceKey.nameKey, URLResourceKey.isDirectoryKey]
-        let options: FileManager.DirectoryEnumerationOptions = [.skipsHiddenFiles, .skipsPackageDescendants, .skipsSubdirectoryDescendants]
-        
-        guard let enumerator = fileManager.enumerator(at: url, includingPropertiesForKeys: keys, options: options, errorHandler: nil) else { return [] }
-        
-        var result: [CacheFile] = []
-        for case let fileURL as URL in enumerator {
-            let name = fileURL.lastPathComponent
-            let cachePath = fileURL.appendingPathComponent("Logs/Build/Cache.db").path
-            
-            if let properties = try? fileManager.attributesOfItem(atPath: cachePath),
-                let modificationDate = properties[FileAttributeKey.modificationDate] as? Date {
-                result.append(CacheFile(name: name, path: cachePath, modificationDate: modificationDate))
-            }
-        }
-        return result
-    }
-    
-    static private func filterFiles(_ files: [CacheFile], byEarliestDate date: Date) -> [CacheFile] {
-        guard files.count > 0 else { return [] }
-        
-        let sortedFiles = files.sorted(by: { $0.modificationDate > $1.modificationDate })
-        let recentFiles = sortedFiles.filter({ $0.modificationDate > date })
-        if recentFiles.count == 0 {
-            return [sortedFiles[0]]
-        }
-        return recentFiles
-    }
 }
